9d5176b015c80d37d4428f009baa6b4958e404f0
Nguyễn Vinh Quang <112387320+quangnv1911@users.noreply.github.com>
2024-11-10 02:49:58 +0700

    feat:initial base project

---
+++ back-end/src/test/java/com/social_pro/backend/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social_pro.backend.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
