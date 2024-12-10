737e5c5858e3c4a65452ac13740a4345af113582
quangnv1911 <quangnv1911@gmail.com>
2024-12-10 05:01:35 -0500

    feat: create handle exception in backend

---
+++ back-end/src/main/java/com/social_pro/backend/backend/entity/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.social_pro.backend.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    UUID id;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    String createdBy;
+
+    @Column(name = "updated_by")
+    @LastModifiedBy
+    String updateBy;
+
+    @Column(name = "create_at")
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    @Column(name = "update_at")
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+}
