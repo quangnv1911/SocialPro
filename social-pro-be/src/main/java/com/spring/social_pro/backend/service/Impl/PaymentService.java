819a1bf13e0d1e2f0625a889b819d377a90ca082
quangnv1911 <quangnv1911@gmail.com>
2025-02-15 16:42:03 +0700

    ci: udpate jenkinsfile and fix bug make build docker failed

--- social-pro-be/src/main/java/com/spring/social_pro/backend/service/Impl/PaymentService.java
+++ social-pro-be/src/main/java/com/spring/social_pro/backend/service/Impl/PaymentService.java
@@ -3,6 +3,8 @@ package com.spring.social_pro.backend.service.Impl;
 import com.spring.social_pro.backend.dto.request.payment.PaymentCallbackRequest;
 import com.spring.social_pro.backend.dto.request.payment.PaymentGenerateRequest;
 import com.spring.social_pro.backend.dto.response.payment.GeneratePaymentResponse;
+import com.spring.social_pro.backend.entity.User;
+import com.spring.social_pro.backend.repository.UserRepository;
 import com.spring.social_pro.backend.service.IPaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.NonFinal;
@@ -11,6 +13,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -22,14 +29,15 @@ public class PaymentService implements IPaymentService {
     @Value("${payment.expire_time}")
     Long PAYMENT_EXPIRE_TIME;
 
-    RedisTemplate<Object, Object> keyDbTemplate;
+    RedisTemplate<String, GeneratePaymentResponse> keyDbTemplate;
+    UserRepository userRepository;
 
     @Override
-    public GeneratePaymentResponse generateToken(PaymentGenerateRequest request,String userId) {
-        String content = "Payment: " + userId;
+    public GeneratePaymentResponse generateToken(PaymentGenerateRequest request, UUID userId) {
+        String content = "Payment: " + generateRandomOtp();
 
         // Tạo dữ liệu giao dịch
-        GeneratePaymentResponse transaction = new GeneratePaymentResponse(request.getGate(), request.getAmount(), content);
+        GeneratePaymentResponse transaction = new GeneratePaymentResponse(request.getGate(), request.getAmount(), content, userId);
 
         // Lưu vào Redis (hết hạn sau 15 phút)
         keyDbTemplate.opsForValue().set(content, transaction, PAYMENT_EXPIRE_TIME, TimeUnit.MINUTES);
@@ -38,11 +46,27 @@ public class PaymentService implements IPaymentService {
 
     @Override
     public void handlePaymentCallback(PaymentCallbackRequest request) {
-        String key = "Payment:" + email;
-        String storedOtp = keyDbTemplate.opsForValue().get(key);
-        if (storedOtp != null && storedOtp.equals(otpInput)) {
-            // Sau khi xác thực, có thể xóa OTP khỏi Redis
-            keyDbTemplate.delete(key);
+        String key = request.getPayment().getContent();
+        GeneratePaymentResponse transactionData = keyDbTemplate.opsForValue().get(key);
+        if (transactionData != null && Objects.equals(transactionData.getAmount(), request.getPayment().getAmount())) {
+            Optional<User> user =  userRepository.findById(transactionData.getUserId());
+            if(user.isPresent()) {
+                User updatedUser = user.get();
+                updatedUser.setMoney(updatedUser.getMoney() + request.getPayment().getAmount());
+                userRepository.save(updatedUser);
+                // Sau khi xác thực, có thể xóa OTP khỏi Redis
+                keyDbTemplate.delete(key);
+            }
+
         }
+    }
+
+    private String generateRandomOtp() {
+        int byteLength = 6;
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] tokenBytes = new byte[byteLength];
+        secureRandom.nextBytes(tokenBytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(tokenBytes);
+
     }
 }
